45db59eab1c791f6d213c3e11a046071cc52f13b
Emerson Gomes <emerson.gomes@thalesgroup.com>
2025-12-03 12:02:57 -0600

    db: remove duplicate chunk_stats deletion in delete_documents_complete__no_commit (#5792)

--- backend/onyx/db/document.py
+++ backend/onyx/db/document.py
@@ -684,11 +684,6 @@ def delete_documents_complete__no_commit(
         document_ids=document_ids,
     )
 
-    delete_chunk_stats_by_connector_credential_pair__no_commit(
-        db_session=db_session,
-        document_ids=document_ids,
-    )
-
     delete_documents_by_connector_credential_pair__no_commit(db_session, document_ids)
     delete_document_feedback_for_documents__no_commit(
         document_ids=document_ids, db_session=db_session
